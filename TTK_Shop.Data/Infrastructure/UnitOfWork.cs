99c311776c16a576743e97da27576ba6f192a9fc
TonKha12345 <trantonkha1999@gmail.com>
2023-08-29 09:03:39 +0700

    New update

--- TTK_Shop.Data/Infrastructure/UnitOfWork.cs
+++ TTK_Shop.Data/Infrastructure/UnitOfWork.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace TTK_Shop.Data.Infrastructure
+﻿namespace TTK_Shop.Data.Infrastructure
 {
     public class UnitOfWork : IUnitOfWork
     {
@@ -20,6 +14,7 @@ namespace TTK_Shop.Data.Infrastructure
         {
             get { return dbContext ?? (dbContext = dbFactory.Init()); }
         }
+
         public void Commit()
         {
             dbContext.SaveChanges();
