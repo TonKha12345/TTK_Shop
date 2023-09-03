74edc5a61148cd5341ff579381a938d078a29ab9
TonKha12345 <trantonkha1999@gmail.com>
2023-09-03 10:01:54 +0700

    $scope and $rootScope

---
+++ TTK_Shop.Wed/Scripts/spa/app.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="../plugins/angular/ajax.googleapis.com_ajax_libs_angularjs_1.8.2_angular.js" />
+
+var myApp = angular.module('myModule', []);
+
+myApp.controller('schoolController', schoolController);
+myApp.controller('studentController', studentController);
+myApp.controller('teacherController', teacherController);
+//DECLARE
+
+function schoolController($scope) {
+    $scope.message = "This is my message from school";
+}
+
+function studentController($scope) {
+    $scope.message = "This is my message from student";
+}
+
+function teacherController($scope) {
+    $scope.message = "This is my message from teacher";
+}
